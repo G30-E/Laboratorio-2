313e6be75f26a31f3d10b947cd26214e23965b98
G30-E <gfloriane@miumg.edu.gt>
2025-02-14 22:51:59 -0600

    Primer ejercicio subido

---
+++ Ejercicio 1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double numeroDecimal;
+        string texto;
+        bool valorBooleano;
+
+
+        Console.Write("Ingresa un número entero: ");
+        int numeroEntero = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingresa un número decimal: ");
+        numeroDecimal = double.Parse(Console.ReadLine());
+
+        Console.Write("Ingresa un texto: ");
+        texto = Console.ReadLine();
+
+        Console.Write("Ingresa un valor verdadero o falso (true/false): ");
+        valorBooleano = bool.Parse(Console.ReadLine());
+
+        
+        Console.WriteLine("\nValores ingresados:");
+        Console.WriteLine("EL número entero ingresado es: " + numeroEntero);
+        Console.WriteLine("El número decimal ingresado es: " + numeroDecimal);
+        Console.WriteLine("El texto ingresado es: " + texto);
+        Console.WriteLine("El valor booleano ingresado es: " + valorBooleano);
+    }
+}
