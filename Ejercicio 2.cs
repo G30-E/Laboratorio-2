2f228b2452b438da437505997c38ed97d5a12e17
G30-E <gfloriane@miumg.edu.gt>
2025-02-14 23:02:39 -0600

    Tercer ejercicio subido

--- Ejercicio 2.cs
+++ Ejercicio 2.cs
@@ -1,76 +1,65 @@
 ﻿using System;
 
-// Declaramos las variables numéricas de diferentes tipos
 int numInt1, numInt2;
 decimal numDecimal1, numDecimal2;
 float numFloat1, numFloat2;
 short numInt16_1, numInt16_2;
 byte numByte1, numByte2;
 
-// Pedimos los valores al usuario para cada tipo de dato
 Console.WriteLine("Ingresa dos números enteros (int): ");
 Console.Write("Primer número (int): ");
 numInt1 = int.Parse(Console.ReadLine());
 Console.Write("Segundo número (int): ");
 numInt2 = int.Parse(Console.ReadLine());
 
-// Realizamos las operaciones con int
 Console.WriteLine("\nOperaciones con enteros (int):");
 Console.WriteLine($"Suma: {numInt1} + {numInt2} = {numInt1 + numInt2}");
 Console.WriteLine($"Resta: {numInt1} - {numInt2} = {numInt1 - numInt2}");
 Console.WriteLine($"Multiplicación: {numInt1} * {numInt2} = {numInt1 * numInt2}");
 Console.WriteLine($"División: {numInt1} / {numInt2} = {(double)numInt1 / numInt2}");
 
-// Pedimos valores decimales
 Console.WriteLine("\nIngresa dos números decimales (decimal): ");
 Console.Write("Primer número (decimal): ");
 numDecimal1 = decimal.Parse(Console.ReadLine());
 Console.Write("Segundo número (decimal): ");
 numDecimal2 = decimal.Parse(Console.ReadLine());
 
-// Realizamos operaciones con decimal
 Console.WriteLine("\nOperaciones con decimales (decimal):");
 Console.WriteLine($"Suma: {numDecimal1} + {numDecimal2} = {numDecimal1 + numDecimal2}");
 Console.WriteLine($"Resta: {numDecimal1} - {numDecimal2} = {numDecimal1 - numDecimal2}");
 Console.WriteLine($"Multiplicación: {numDecimal1} * {numDecimal2} = {numDecimal1 * numDecimal2}");
 Console.WriteLine($"División: {numDecimal1} / {numDecimal2} = {numDecimal1 / numDecimal2}");
 
-// Pedimos valores float
 Console.WriteLine("\nIngresa dos números de punto flotante (float): ");
 Console.Write("Primer número (float): ");
 numFloat1 = float.Parse(Console.ReadLine());
 Console.Write("Segundo número (float): ");
 numFloat2 = float.Parse(Console.ReadLine());
 
-// Realizamos operaciones con float
 Console.WriteLine("\nOperaciones con float:");
 Console.WriteLine($"Suma: {numFloat1} + {numFloat2} = {numFloat1 + numFloat2}");
 Console.WriteLine($"Resta: {numFloat1} - {numFloat2} = {numFloat1 - numFloat2}");
 Console.WriteLine($"Multiplicación: {numFloat1} * {numFloat2} = {numFloat1 * numFloat2}");
 Console.WriteLine($"División: {numFloat1} / {numFloat2} = {numFloat1 / numFloat2}");
 
-// Pedimos valores de tipo short (int16)
 Console.WriteLine("\nIngresa dos números enteros pequeños (int16): ");
 Console.Write("Primer número (int16): ");
 numInt16_1 = short.Parse(Console.ReadLine());
 Console.Write("Segundo número (int16): ");
 numInt16_2 = short.Parse(Console.ReadLine());
 
-// Realizamos operaciones con int16
 Console.WriteLine("\nOperaciones con int16:");
 Console.WriteLine($"Suma: {numInt16_1} + {numInt16_2} = {numInt16_1 + numInt16_2}");
 Console.WriteLine($"Resta: {numInt16_1} - {numInt16_2} = {numInt16_1 - numInt16_2}");
 Console.WriteLine($"Multiplicación: {numInt16_1} * {numInt16_2} = {numInt16_1 * numInt16_2}");
 Console.WriteLine($"División: {numInt16_1} / {numInt16_2} = {(double)numInt16_1 / numInt16_2}");
 
-// Pedimos valores de tipo byte
 Console.WriteLine("\nIngresa dos números pequeños (byte): ");
 Console.Write("Primer número (byte): ");
 numByte1 = byte.Parse(Console.ReadLine());
 Console.Write("Segundo número (byte): ");
 numByte2 = byte.Parse(Console.ReadLine());
 
-// Realizamos operaciones con byte
 Console.WriteLine("\nOperaciones con byte:");
 Console.WriteLine($"Suma: {numByte1} + {numByte2} = {numByte1 + numByte2}");
 Console.WriteLine($"Resta: {numByte1} - {numByte2} = {numByte1 - numByte2}");
